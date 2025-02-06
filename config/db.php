3d52ab4c5f70123113a22627c4d34d5450a9cb8d
DICH <antonlykinov96@gmail.com>
2025-02-06 22:04:25 +0300

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=dichdb',
+    'username' => 'root',
+    'password' => '', // Убедитесь, что пароль указан правильно
+    'charset' => 'utf8',
+];
+
+
+
